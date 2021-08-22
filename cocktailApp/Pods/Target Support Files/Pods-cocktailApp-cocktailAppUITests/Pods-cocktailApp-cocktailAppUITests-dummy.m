7ec455add50d31d3386c0eea4cf8a0702d01342a
Max Vakhbe <maxim.vakhbe@gmail.com>
2021-08-22 22:43:04 +0300

    Начальная стадия проекта
    Загрузка начального проекта

---
+++ cocktailApp/Pods/Target Support Files/Pods-cocktailApp-cocktailAppUITests/Pods-cocktailApp-cocktailAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cocktailApp_cocktailAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_cocktailApp_cocktailAppUITests
+@end
---
+++ cocktailApp/cocktailApp/MenuViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MenuViewController.swift
+//  cocktailApp
+//
+//  Created by Mix174 on 22.08.2021.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
